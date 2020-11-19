ecec3efc859bd2715bc0e364c74bccfb48173d90
nimtabo <nicholasmtabo@yahoo.com>
2020-11-19 13:54:45 +0300

    update proxy port

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const port = process.env.PORT || 5000;
 
 module.exports = function (app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: `http://localhost:${port}`,
+      changeOrigin: true,
+    })
+  );
 };
