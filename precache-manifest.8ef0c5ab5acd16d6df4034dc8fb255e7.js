9ebd3db79030c7ee88f0250f7c9040aac4349df6
nimtabo <nicholasmtabo@yahoo.com>
2020-11-19 20:08:35 +0300

    Updates

---
+++ precache-manifest.8ef0c5ab5acd16d6df4034dc8fb255e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6750afef8b5f17924f18113630574a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe0034238badf534e63",
+    "url": "/static/css/2.37050330.chunk.css"
+  },
+  {
+    "revision": "e5d78b58514874d3fde0",
+    "url": "/static/css/main.4ded793a.chunk.css"
+  },
+  {
+    "revision": "afe0034238badf534e63",
+    "url": "/static/js/2.3ac9bea7.chunk.js"
+  },
+  {
+    "revision": "fa84c71a359f6722cd67a0e34a49761e",
+    "url": "/static/js/2.3ac9bea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d78b58514874d3fde0",
+    "url": "/static/js/main.ba6f46eb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
