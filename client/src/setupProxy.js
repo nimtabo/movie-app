933f1dc0f7aaf5e2ce4677ee33b82cf5268aca32
nimtabo <nicholasmtabo@yahoo.com>
2020-11-21 15:11:19 +0300

    heroku scripts

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,18 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const API_URL = "https://themoviedbapp.herokuapp.com/";
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+// module.exports = function (app) {
+//   const API_SOURCE_URL = "https://themoviedbapp.herokuapp.com/";
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: API_SOURCE_URL,
+//       changeOrigin: true,
+//     })
+//   );
+// };
+
+const proxy = require("http-proxy-middleware");
+const port = process.env.PORT || 5000;
+
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: API_URL,
-      changeOrigin: true,
-    })
-  );
+  app.use(proxy("/api", { target: `http://localhost:${port}/` }));
 };
